2b862f90ac5cbdc7f8ef75ab757ef9093fe743b7
leoking <Leoking470@gmail.com>
2020-12-24 12:35:44 +0200

    added minimap

--- Raycasting_SGG/Game.cpp
+++ Raycasting_SGG/Game.cpp
@@ -26,9 +26,41 @@ void Game::Update()
 void Game::Draw()
 {
 	camera.Render();
-	
-	//FPS counter
+
 	graphics::Brush br;
+
+	if (graphics::getKeyState(graphics::SCANCODE_M))
+	{
+		/////////////////// minimap //////////////////
+		br.fill_color[0] = 0.0;
+		br.fill_color[1] = 0.5;
+		br.fill_color[2] = 0.5f;
+		br.outline_opacity = 0.0f;
+
+		for (int y = 0; y < level.GetHeight(); y++)
+		{
+			for (int x = 0; x < level.GetWidth(); x++)
+			{
+				if (level.Get(x, y) != ' ')
+				{
+					graphics::drawRect(float(x * 6 + 3), float(y * 6 + 3), 5.0f, 5.0f, br);
+				}
+			}
+		}
+
+		br.fill_color[0] = 1.0f;
+		br.fill_color[1] = 0.0f;
+		br.fill_color[2] = 0.0f;
+
+		Vector2 v = level.GetPlayer().Position() + level.GetPlayer().Direction();
+
+		graphics::drawDisk(level.GetPlayer().Position().x * 6 + 3, level.GetPlayer().Position().y * 6 + 3, 2, br);
+		br.outline_opacity = 1.0f;
+		graphics::drawLine(level.GetPlayer().Position().x * 6 + 3, level.GetPlayer().Position().y * 6 + 3,
+			v.x * 6 + 3, v.y * 6 + 3, br);
+	}
+
+	//FPS counter
 	br.fill_color[0] = 1.0;
 	br.fill_color[1] = 1.0;
 	br.fill_color[2] = 1.0f;
